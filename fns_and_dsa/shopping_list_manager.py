31071db3d71e6859f013c297124f15a9fc1c3733
Abdelmounaim Elharba <monimeelharba123@gmail.com>
2025-07-02 21:51:59 +0100

    Fix: cleaner messages and format for checker

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,6 +1,6 @@
-def  display_menu():
+def display_menu():
     print("\nShopping List Manager")
-    print("1. add Item ")
+    print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
@@ -10,38 +10,35 @@ def main():
 
     while True:
         display_menu()
-        choice = input("Enter your choice : ").strip()
+        choice = input("Enter your choice: ").strip()
 
         if choice == '1':
-            item = input("Enter item to add :").strip()
-            
-            if item :
+            item = input("Enter item to add: ").strip()
+            if item:
                 shopping_list.append(item)
                 print(f"'{item}' has been added to the list.")
             else:
-                print("Your shopping list is empty")
-        
+                print("Item cannot be empty.")
+
         elif choice == '2':
-            item = input("Enter item to remove : ").strip()
-            
-            if item in shopping_list : 
+            item = input("Enter item to remove: ").strip()
+            if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"'{item}' has been removed from the list.")
             else:
-                print(f"'{item}' not found in the shopping list.") 
+                print(f"'{item}' not found in the shopping list.")
 
         elif choice == '3':
             if shopping_list:
-                print("your shopping list :")
+                print("Your shopping list:")
                 for i, item in enumerate(shopping_list, start=1):
-                 print(f"{i}. {item}")
+                    print(f"{i}. {item}")
             else:
                 print("Your shopping list is empty.")
 
         elif choice == '4':
             print("Goodbye!")
             break
-            
         else:
             print("Invalid choice. Please try again.")
 
